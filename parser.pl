608c595a7c3f783b4f4ffe07f8d9730352e41efe
Juan Spinel <juan@spinel.ch>
2021-01-11 19:18:53 +0100

    Parser removes 404 errors

--- parser.pl
+++ parser.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+use HTTP::Tiny;
+
 my $sherlock = "sherlock.txt";
 my $whatsmyname = "whatsmyname.txt";
 
@@ -16,24 +18,35 @@ while (my $line = <$fh>){
     chomp $line;
     $line =~ s/www.//;
     $sites{$line}++ unless $line !~ /https/;
+    $counter++ unless $line !~ /https/;
     }
 close $fh;
 
 open ($fh, '<', $whatsmyname) or die $!;
 while (my $line = <$fh>){
     chomp $line;
-    $line =~ s/www.//;
-    $sites{$line}++;
+    $sites{$line}++ unless $line !~ /https/;
+    $counter++ unless $line !~ /https/;
     }
 close $fh;
 
-print("Total sites: $counter\n");
-my $unique = keys %sites;
-print("Unique sites: $unique\n");
+
+
 
 @sites = sort keys %sites;
 
 open ($fh, '>', 'unique.txt') or die $!;
 foreach (@sites){
-    print $fh "$_\n";
+    my $response = HTTP::Tiny->new->get($_);
+    if ($response->{success}){
+        print $fh "$_\n";
     }
+    else{
+        print("Failed request on $_\n");
+        delete($sites{$_});
+    }
+}
+
+my $unique = keys %sites;
+print("Total results: $counter\n");
+print("Unique sites: $unique\n");
